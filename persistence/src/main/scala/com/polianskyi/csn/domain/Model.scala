994826c870c07ec7d740ba1cfe7cfb1a0ef94d17
VadymPolyanski <vadym.polyanski@gmail.com>
2018-04-01 00:23:37 +0300

    Created coffee drink model

--- persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
+++ persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
@@ -3,6 +3,8 @@ package com.polianskyi.csn.domain
 import java.sql.Date
 
 final case class CoffeeHouse(address: String, space: Double, rentalPrice: Double, mobileNumber: String)
+final case class CoffeeDrink(name: String, price: Double, nativePrice: Double, products: List[Product], description: String)
+final case class Product(name: String, price: Double, unit_of_measurement: String, description: String)
 final case class Employee(fullName: String,
                           birthdayDate: Date,
                           mobileNumber: String,
