5c889fcb6915f5fbad2a4cbb637aabc5c2a1e1e3
VadymPolyanski <vadym.polyanski@gmail.com>
2018-04-01 16:33:52 +0300

    added standart products

---
+++ persistence/src/main/resources/db/migration/V1.3__Add_standart_products.sql
@@ -0,0 +1,32 @@
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('water', 0.5, 'liter', 'Clear water is needed for all coffee drinks');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('coffee beans', 20, 'kilogram', 'Coffee beans is our main product');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('suggar', 12, 'kilogram', 'Somebody can use it for any coffee drinks');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('milk', 12, 'liter', 'Latte, capucino and another coffee drinks need this');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('syrup', 35, 'liter', 'People use it like suggar with extra aromat');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('disposable cups 35-40', 13, 'pack', 'Cups for espresso');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('disposable cups 140-220', 18, 'pack', 'Cups for cappuccino');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('disposable cups 220-230', 20, 'pack', 'Cups for americano');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('disposable cups 150-300', 24, 'pack', 'Cups for latte');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('wooden sticks', 8, 'pack', 'For blending');
+
+INSERT INTO products (name, price, unit_of_measurement, description)
+VALUES ('napkins', 10, 'pack', 'Anybody can use it for cleaning');
