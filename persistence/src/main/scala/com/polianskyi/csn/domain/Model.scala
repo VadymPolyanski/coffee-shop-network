b4d6a03520d5da25f35d593e366a64f93afe59c7
VadymPolyanski <vadym.polyanski@gmail.com>
2018-04-01 12:55:17 +0300

    corrected name

--- persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
+++ persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
@@ -4,7 +4,7 @@ import java.sql.Date
 
 final case class CoffeeHouse(address: String, space: Double, rentalPrice: Double, mobileNumber: String)
 final case class CoffeeDrink(name: String, price: Double, nativePrice: Double, products: List[Product], description: String)
-final case class Product(name: String, price: Double, unit_of_measurement: String, description: String)
+final case class Product(name: String, price: Double, unitOfMeasurement: String, description: String)
 final case class Employee(fullName: String,
                           birthdayDate: Date,
                           mobileNumber: String,
