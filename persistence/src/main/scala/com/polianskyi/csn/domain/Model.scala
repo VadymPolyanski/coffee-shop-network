72b1c9a637955c6a925d521fb0520fa899d9dc05
VadymPolyanski <vadym.polyanski@gmail.com>
2018-04-03 12:50:33 +0300

    Created sales report db service

--- persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
+++ persistence/src/main/scala/com/polianskyi/csn/domain/Model.scala
@@ -2,9 +2,9 @@ package com.polianskyi.csn.domain
 
 final case class CoffeeHouse(address: String, space: Double, rentalPrice: Double, mobileNumber: String)
 final case class CoffeeDrink(name: String, price: Double, nativePrice: Double, products: List[Product], description: String)
-final case class SalesReport(coffee_drink: CoffeeDrink, employee: Employee, priceWithVat: Double, saleDate: Long, coffeeHouse: CoffeeHouse)
+final case class SalesReport(coffeeDrink: CoffeeDrink, employee: Employee, priceWithVat: Double, saleDate: Long, coffeeHouse: CoffeeHouse)
 final case class Product(name: String, price: Double, unitOfMeasurement: String, description: String)
-final case class Position(name: String, avg_salary: Double, priority: Integer, description: String)
+final case class Position(name: String, avgSalary: Double, priority: Integer, description: String)
 
 final case class Contract(contractNumber: Int,
                           workPosition: String,
@@ -23,4 +23,4 @@ final case class Employee(fullName: String,
                           passport: String,
                           sex: String)
 
-final case class SalesReportPK(coffee_drink: CoffeeDrink, employee: Employee, saleDate: Long)
+final case class SalesReportPK(coffeeDrink: String, employee: String, saleDate: Long)
